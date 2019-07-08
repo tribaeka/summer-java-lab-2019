e53b18901bdd45ec120e3955fd9b18533b7de557
Yahor Hlushak <egor924462@gmail.com>
2019-07-08 14:30:57 +0000

    Runner.java зафиналил переменные

--- task-1/src/main/java/by/epam/training/task1/Runner.java
+++ task-1/src/main/java/by/epam/training/task1/Runner.java
@@ -7,10 +7,10 @@ import java.util.Locale;
 
 public class Runner {
     public static void main(String[] args) {
-        String helloString = "Hello word: ";
-
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMM ss HH yyyy mm", Locale.ENGLISH);
-        LocalDateTime date = LocalDateTime.now();
+        final String helloString = "Hello word: ";
+        final String datePattern = "dd MMM ss HH yyyy mm";
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(datePattern, Locale.ENGLISH);
+        final LocalDateTime date = LocalDateTime.now();
 
         System.out.println(helloString + dateTimeFormatter.format(date));
         System.out.println(helloString + dateTimeFormatter.format(date.plusWeeks(1)));
