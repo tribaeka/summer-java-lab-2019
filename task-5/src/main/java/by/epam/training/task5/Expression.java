f43dfa0cc3844ab78b04ca4b1b040051ccd98ea4
tribaeka <egor924462@gmail.com>
2019-07-21 15:18:27 +0300

    task-5: new logic splitting test

--- task-5/src/main/java/by/epam/training/task5/Expression.java
+++ task-5/src/main/java/by/epam/training/task5/Expression.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class Expression {
     private String defaultExpression;
     private List<List<Integer>> results;
-
+    private static int[] iArr = new int[100];
+    private static List<String> splittingList = new LinkedList<>();
     public Expression(){
 
     }
@@ -14,7 +15,30 @@ public class Expression {
         this.results = null;
     }
 
+    public String[] solveExpression(){
+        String[] expression = defaultExpression.split(" ");
+        int sum = Integer.parseInt(expression[0]);
+        int[] elements = new int[expression.length - 2];
+        return null;
+    }
+    public void calcSplitting(int n, int k, int i) {
 
+        if ( n < 0 ) return;
+        if ( n == 0 ) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < i; j++) {
+                stringBuilder.append(iArr[j]).append(" ");
+            }
+            splittingList.add(stringBuilder.toString());
+        }
+        else {
+            if ( n >= k) {
+                iArr[i] = k;
+                calcSplitting(n - k, k, i + 1);
+            }
+            if ( k > 1) calcSplitting(n, k - 1, i);
+        }
+    }
 
     public String getDefaultExpression() {
         return defaultExpression;
@@ -30,5 +54,9 @@ public class Expression {
             integerList.add(Integer.parseInt(item));
         }
         return integerList;
+    }
+
+    public List<String> getSplittingList() {
+        return splittingList;
     }
 }
--- task-5/src/test/java/by/epam/training/task5/ExpressionTest.java
+++ task-5/src/test/java/by/epam/training/task5/ExpressionTest.java
@@ -3,9 +3,7 @@ package by.epam.training.task5;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -15,6 +13,25 @@ public class ExpressionTest {
     @Before
     public void init(){
         expression = new Expression();
+    }
+
+    @Test
+    public void basicCalcSplitting(){
+        List<String> expected = new LinkedList<>(Arrays.asList(
+                "6 ",
+                "5 1 ",
+                "4 2 ",
+                "4 1 1 ",
+                "3 3 ",
+                "3 2 1 ",
+                "3 1 1 1 ",
+                "2 2 2 ",
+                "2 2 1 1 ",
+                "2 1 1 1 1 ",
+                "1 1 1 1 1 1 "));
+        expression.calcSplitting(6, 6 ,0);
+        assertThat(expression.getSplittingList(), is(expected));
+
     }
 
 }
